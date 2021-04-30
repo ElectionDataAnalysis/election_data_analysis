cd23ca00b9a3fb2ed08a0359c97b0f501333daec
Todd Graham <toddgraham121@gmail.com>
2021-04-30 12:33:32 -0700

    American Samoa loads into DB, but fails vote tests

---
+++ tests/American-Samoa/test_American-Samoa_2020-General_Official.py
@@ -0,0 +1,137 @@
+import election_data_analysis as e
+
+# Instructions:
+#   Change the constants to values from your file
+#   Delete any tests for contest types your state doesn't have in 2020 (e.g., Florida has no US Senate contest)
+#   (Optional) Change district numbers
+#   Replace each '-1' with the correct number calculated from the results file.
+#   Move this testing file to the correct jurisdiction folder in `election_data_analysis/tests`
+
+# # # constants - CHANGE THESE!! - use internal db names
+election = "2020 General"
+jurisdiction = "American Samoa"
+abbr = "AS"
+total_pres_votes = -1  # total of all votes for President
+total_gov_votes = 11861  # total of all votes for Governor
+cd = 1  # congressional district
+total_cd_votes = 11749  # total votes in the chosen cd
+shd = "1"  # state house district
+total_shd_votes = -1
+ssd = 14  # state senate district
+total_ssd_votes = -1
+single_vote_type = "total"  # pick any one from your file
+con_votes_vote_type = 11749
+# pick any one from your file
+single_county = "American Samoa;Dist #5 - Sua No. 1"
+con_votes_county = 493  # total votes for congress of that county
+
+
+def test_data_exists(dbname):
+    assert e.data_exists(election, jurisdiction, dbname=dbname)
+
+
+'''
+def test_presidential(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"US President ({abbr})",
+            dbname=dbname,
+        )
+        == total_pres_votes
+    )
+'''
+
+
+def test_governor_totals(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"{abbr} Governor",
+            dbname=dbname,
+        )
+        == total_gov_votes
+    )
+
+
+def test_congressional_totals(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"US House {abbr} District {cd}",
+            dbname=dbname,
+        )
+        == total_cd_votes
+    )
+
+
+'''
+def test_state_senate_totals(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"{abbr} Senate District {ssd}",
+            dbname=dbname,
+        )
+        == total_ssd_votes
+    )
+
+
+def test_state_house_totals(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"{abbr} House District {shd}",
+            dbname=dbname,
+        )
+        == total_shd_votes
+    )
+'''
+
+
+def test_standard_vote_types(dbname):
+    assert e.check_count_types_standard(election, jurisdiction, dbname=dbname)
+
+
+def test_vote_type_counts_consistent(dbname):
+    assert e.check_totals_match_vote_types(
+        election, jurisdiction, dbname=dbname)
+
+
+def test_count_type_subtotal(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"US House {abbr} District {cd}",
+            dbname=dbname,
+            vote_type=single_vote_type,
+        )
+        == con_votes_vote_type
+    )
+
+
+def test_county_subtotal(dbname):
+    assert (
+        e.contest_total(
+            election,
+            jurisdiction,
+            f"US House {abbr} District {cd}",
+            dbname=dbname,
+            county=single_county,
+        )
+        == con_votes_county
+    )
+
+
+def test_all_candidates_known(dbname):
+    assert (
+        e.get_contest_with_unknown_candidates(
+            election, jurisdiction, dbname=dbname)
+        == []
+    )
